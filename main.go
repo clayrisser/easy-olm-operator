b371835380e130f16fd991de664ecf73ee1ca8f4
Clay Risser <clayrisser@gmail.com>
2023-06-27 16:47:15 -0400

    ✨ feat: add RBAC rules for configmaps
    This commit adds new RBAC rules to allow creating, deleting, getting, listing, patching, updating, and watching ConfigMaps. The changes are made in the `rbac.yaml` file under `chart/templates` and `config/rbac`, as well as in `main.go`. The rules are defined using `apiGroups` and `resources` and apply to an empty group and `configmaps`. Additionally, a `kubebuilder` comment was added to `main.go`.

--- main.go
+++ main.go
@@ -52,6 +52,7 @@ func init() {
 }
 
 //+kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch;create;update;patch;delete
 
 func main() {
 	var metricsAddr string
